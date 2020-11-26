a8ba089031548b350c1747ed4ba87d3b7e625073
Natnael Tezera <natnaeltezera@outlook.com>
2020-11-25 21:30:32 -0800

    login validator added

--- springmvcassignment/src/main/java/com/nat/springmvcassignment/controller/AuthController.java
+++ springmvcassignment/src/main/java/com/nat/springmvcassignment/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.nat.springmvcassignment.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.nat.springmvcassignment.model.Login;
 import com.nat.springmvcassignment.repository.LoginRepository;
@@ -23,14 +27,24 @@ public class AuthController {
 	}
 	
 	@PostMapping("/login.html")
-	public String validateLogin(@ModelAttribute Login login) {
+	public ModelAndView validateLogin(@ModelAttribute @Valid Login login, BindingResult result) {
 		System.out.println("hello from validatelogin"+login);
+		ModelAndView modelAndView = new ModelAndView();
+		if(result.hasErrors()) {
+			result.getFieldErrors().forEach(e->{
+				System.out.println(e.getDefaultMessage() + " "+ e.getField());
+
+			});
+			modelAndView.setViewName("login");
+			return modelAndView;
+		}
 		if(login.equals(loginRepository.findById(login.getUserName()).get())) {
 			System.out.println("success");
 		}
 		else {
 			System.out.println("fail");
 		}
-		return "home";
+		modelAndView.setViewName("redirect:/dashboard");
+		return modelAndView;
 	}
 }
--- springmvcassignment/src/main/java/com/nat/springmvcassignment/model/Login.java
+++ springmvcassignment/src/main/java/com/nat/springmvcassignment/model/Login.java
@@ -3,6 +3,7 @@ package com.nat.springmvcassignment.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,7 +16,9 @@ import lombok.NoArgsConstructor;
 public class Login {
 	@Id
 	@Column(length = 20)
+	@Size(max=20, min =3 ,message = "user name is not valid")
 	private String userName;
+	@Size(max=20, min =3 ,message = "passward is not valid")
 	private String password;
 
 }
